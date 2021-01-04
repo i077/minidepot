02d066516676b50b0323447531a4fb561cc10b28
Imran Hossain <hi@imranh.xyz>
2021-01-04 11:33:44 -0500

    ssl: Use DNS-01 verification

--- modules/ssl.nix
+++ modules/ssl.nix
@@ -1,9 +1,19 @@
-{ ... }:
+{ config, ... }:
 
 {
+  sops.secrets.cf-acme-credentials = { };
+
   security.acme = {
     acceptTerms = true;
     email = "acme" + "@" + "imranh.xyz";
-    certs."minidepot.imranh.xyz" = {};
+    certs."minidepot.imranh.xyz" = {
+      dnsPropagationCheck = true;
+      dnsProvider = "cloudflare";
+      credentialsFile = config.sops.secrets.cf-acme-credentials.path;
+    };
   };
+  systemd.services."acme-minidepot.imranh.xyz".serviceConfig.SupplementaryGroups =
+    [ config.users.groups.keys.name ];
+  
+  users.users.acme.extraGroups = [ config.users.groups.keys.name ];
 }
